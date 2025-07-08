1f5f47f1a5213325542240ebcd023cb94e33451f
JohnKamaujk <johnjkkamau@gmail.com>
2025-07-08 18:41:59 +0300

    feat: setup get all users route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI, HTTPException 
+from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routes import users
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message":"Hello, WOrld!"}
+app.include_router(users.router)
